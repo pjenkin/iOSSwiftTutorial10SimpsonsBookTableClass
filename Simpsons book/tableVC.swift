c31756dcbd84dccd3ee588de7922b04e140d0f37
Peter Jenkin <peternjenkin@gmail.com>
2019-04-06 10:52:51 +0100

    10-73 Setting up 2 views (table on 1st, image view on 2nd)

---
+++ Simpsons book/tableVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Simpsons book
+//
+//  Created by Peter Jenkin on 05/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class tableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
